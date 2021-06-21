4a9c4a3cd33303792d171059a1ad524b9369ca02
wahsandaruwan <wahsandaruwan6@gmail.com>
2021-06-21 17:47:10 +0530

    Code updated

--- scripts.js
+++ scripts.js
@@ -1,21 +1,26 @@
-// Variables
+// -----Variables-----
 var slide = document.querySelectorAll('.slide');
 var rbtn = document.querySelectorAll('.mn-btn');
 
-// Manual Buttons Navigation
+// -----Click Event for Manual Button-----
 rbtn.forEach(function(item, index){
     item.addEventListener('click', function(){
         console.log(index);
-        for(var i = 0; i < slide.length; i++){
-            if(i !== index){
-                console.log(i);
-                slide[i].style.display = "none";
-                rbtn[i].classList.remove("active");
-            }
-            else{
-                slide[index].style.display = "";
-                rbtn[index].classList.add("active");
-            }
-        }
+        radioNav(index);
     });
-})
+});
+
+// -----Funtion for Manual Button Navigation-----
+function manButtonNav(index){
+    for(var i = 0; i < slide.length; i++){
+        if(i !== index){
+            console.log(i);
+            slide[i].style.display = "none";
+            rbtn[i].classList.remove("active");
+        }
+        else{
+            slide[index].style.display = "";
+            rbtn[index].classList.add("active");
+        }
+    }
+}
