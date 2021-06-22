3772fb1276883a7402e554f6a462812a3b43c5c4
wahsandaruwan <wahsandaruwan6@gmail.com>
2021-06-23 00:07:28 +0530

    Auto slide

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 // -----Variables-----
 var slide = document.querySelectorAll('.slide');
 var rbtn = document.querySelectorAll('.mn-btn');
+var count = 1;
+var slideInt;
 
 // -----Click Event for Manual Button-----
 rbtn.forEach(function(item, index){
@@ -15,12 +17,34 @@ function manButtonNav(index){
     for(var i = 0; i < slide.length; i++){
         if(i !== index){
             console.log(i);
-            slide[i].style.display = "none";
+            slide[i].classList.remove("curr");
             rbtn[i].classList.remove("active");
         }
         else{
-            slide[index].style.display = "";
+            slide[index].classList.add("curr");
             rbtn[index].classList.add("active");
         }
     }
+    clearInterval(slideInt);
+    slideInt = setInterval(nextImage, 5000);
 }
+
+// -----Funtion for Navigate Next Image-----
+function nextImage(){
+    var curr = document.querySelector('.curr');
+    var act = document.querySelector('.active');
+    curr.classList.remove('curr');
+    act.classList.remove('active');
+    if(curr.nextElementSibling){
+        curr.nextElementSibling.classList.add('curr');
+        console.log(curr.nextElementSibling)
+        act.nextElementSibling.classList.add('active');
+    }
+    else{
+        slide[0].classList.add('curr');
+        rbtn[0].classList.add('active');
+    }
+}
+
+// Slide Interval
+slideInt = setInterval(nextImage, 5000);
